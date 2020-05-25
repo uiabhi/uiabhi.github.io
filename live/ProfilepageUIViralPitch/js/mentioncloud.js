bcb5779cda4dcb5cd1860eeb0102fb723a3b6d66
Abhishek Singh <47791443+thatdelhiboy@users.noreply.github.com>
2020-05-25 21:09:32 +0530

    updateed charts

--- live/ProfilepageUIViralPitch/js/mentioncloud.js
+++ live/ProfilepageUIViralPitch/js/mentioncloud.js
@@ -1,69 +1,29 @@
 anychart.onDocumentReady(function () {
-    var data = [{
-            "x": "@like4like",
-            "value": 50
-        },
-        {
-            "x": "@memes",
-            "value": 10
-        },
-        {
-            "x": "@Hindustani",
-            "value": 70
-        },
-        {
-            "x": "@viralpitch",
-            "value": 60
-        },
-        {
-            "x": "@friends",
-            "value": 40
-        },
-        {
-            "x": "@loophole",
-            "value": 30
-        },
-        {
-            "x": "@Russian",
-            "value": 21
-        },
-        {
-            "x": "@test",
-            "value": 60
-        },
-        {
-            "x": "@hastags",
-            "value": 40
-        },
-        {
-            "x": "@unkonwn",
-            "value": 30
-        },
-        {
-            "x": "@olelele",
-            "value": 21
-        },
+
+    // create data   
+    var data = [
+{x: "@learning", value: 80},
+{x: "@includes", value: 56},
+{x: "@lists", value: 44},
+{x: "@meaning", value: 40},
+{x: "@useful", value: 36},
+{x: "@different", value: 32},
+{x: "@grammar", value: 28},
+{x: "@teaching", value: 24},
+{x: "@example", value: 20},
+{x: "@thing", value: 12}
     ];
 
-    // create a tag (word) cloud chart
-    var chart = anychart.tagCloud(data);
-
-    // set a chart title
-    // set an array of angles at which the words will be laid out
-    chart.angles([0])
-    // enable a color range
-    chart.colorRange(true);
-    // set the color range length
-    chart.colorRange().length('80%');
-    chart.listen("pointClick", function (e) {
+    // create a chart and set the data
+    var chartt = anychart.tagCloud(data);
+    chartt.angles(1)
+    // set the container id
+    chartt.container("testone");
+    chartt.listen("pointClick", function (e) {
         var url = "https://en.wikipedia.org/wiki/" + e.point.get("x");
         window.open(url, "_blank");
     });
 
-    // display the word cloud chart
-    chart.container("testone");
-    chart.draw();
-
-
-
+    // initiate drawing the chart
+    chartt.draw();
 });
