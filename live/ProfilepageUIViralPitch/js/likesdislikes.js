c0f13a0720b76b9e51197635d4c2ec92867ed007
Abhishek Singh <47791443+thatdelhiboy@users.noreply.github.com>
2020-05-29 12:35:12 +0530

    commit

---
+++ live/ProfilepageUIViralPitch/js/likesdislikes.js
@@ -0,0 +1,35 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Montserrat', 'Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("pvchart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Likes", "Dislike"],
+    datasets: [{
+      data: [5500, 100],
+      backgroundColor: ['#232fbb', '#009500'],
+      hoverBackgroundColor: ['#2320bb', '#009550'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 50,
+  },
+});
---
+++ live/ProfilepageUIViralPitch/js/viewslikes.js
@@ -0,0 +1,35 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Montserrat', 'Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("lcchart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Likes", "Comment"],
+    datasets: [{
+      data: [100, 50],
+      backgroundColor: ['#232fbb', '#009500'],
+      hoverBackgroundColor: ['#2320bb', '#009550'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 50,
+  },
+});
